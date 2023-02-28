3e8cf303a633e74e70b32d5e08f1fbdbbbab57c1
CXoXo-io <126208733+CXoXo-io@users.noreply.github.com>
2023-02-28 10:29:59 -0500

    taking site down

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,10 @@ export default function Home() {
           <input style={styles.input} placeholder="Days" onChange={e => setRequest(request => ({
             ...request, days: e.target.value
           }))} />
-          <button className="input-button"  onClick={hitAPI}>Build Itinerary</button>
+          <button className="input-button"  onClick={nada}>Come back soon!</button>
+          {/*<button className="input-button"  onClick={hitAPI}>Build Itinerary</button>
+          */}
+
         </div>
         <div className="results-container">
         {
