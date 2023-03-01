6a97e746a59c5aa930920f6ea5efb887b8e3eed0
CXoXo-io <126208733+CXoXo-io@users.noreply.github.com>
2023-03-01 03:50:45 -0500

    footer update

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         </div>
         <footer className="footer">
             <p>
-            <a className="sponsor" target="_blank" rel="no-opener" href="https://bit.ly/cjcxoxo"> We've hit our limits. Until we're back, here are flights below $150! 🫡</a>
+            <a className="sponsor" target="_blank" rel="no-opener" href="https://bit.ly/cjcxoxo"> We've hit our limits. Until we're back, enjoy flights below $150 🫡</a>
             </p>
         </footer>
       </body>
