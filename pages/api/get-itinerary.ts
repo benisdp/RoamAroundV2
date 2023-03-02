c7235b2e9558682bc0af2a009c03320d38021a3e
CXoXo-io <126208733+CXoXo-io@users.noreply.github.com>
2023-03-01 19:13:23 -0500

    reversing block

--- pages/api/get-itinerary.ts
+++ pages/api/get-itinerary.ts
@@ -22,13 +22,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | Error>
 ) {
-  let days, city, startDate, block;
+  let days, city, startDate;
   if (req.body) {
     let body = JSON.parse(req.body);
     days = body.days;
     city = body.city;
     startDate = body.startDate;
-    block = body.block;
   }
 
   const parts = city.split(' ');
@@ -42,33 +41,28 @@ export default async function handler(
   }
   let basePrompt = `You are a travel guide that knows everything about ${city}. I am a tourist who has never visited ${city}. What is an ideal itinerary for ${days} days starting on ${startDate} in ${city} that includes all the city's main attractions, some historic and some adventerous? Break out your receommendation for each day into morning, afternoon and evening activities and recommend top rated restaurants. Always keep itineraries to under 500 words.`;
   try {
-    if (!block) {
-      const response = await fetch('https://api.openai.com/v1/completions', {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-          model: 'text-davinci-003',
-          prompt: basePrompt,
-          temperature: 0,
-          max_tokens: 550,
-        }),
-      });
+    const response = await fetch('https://api.openai.com/v1/completions', {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt: basePrompt,
+        temperature: 0,
+        max_tokens: 550
+      })
+    })
       const itinerary = await response.json();
       const pointsOfInterestPrompt =
         'Extract the points of interest out of this text, with no additional words, separated by commas: ' +
         itinerary.choices[0].text;
 
-      res.status(200).json({
-        message: 'success',
-        pointsOfInterestPrompt,
-        itinerary: itinerary.choices[0].text,
-      });
-    } else {
-      res.status(404).json({
-        message: 'failure',
-      });
+        res.status(200).json({
+          message: 'success',
+          pointsOfInterestPrompt,
+          itinerary: itinerary.choices[0].text
+        })
+    
+      } catch (err) {
+        console.log('error: ', err)
+      }
     }
-  } catch (err) {
-    console.log('error: ', err);
-  }
-}
--- pages/api/get-points-of-interest.ts
+++ pages/api/get-points-of-interest.ts
@@ -17,37 +17,27 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { pointsOfInterestPrompt, block } = JSON.parse(req.body);
 
-  try {
-    if (!block) {
-      const response2 = await fetch('https://api.openai.com/v1/completions', {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-          model: 'text-davinci-003',
-          prompt: pointsOfInterestPrompt,
-          temperature: 0,
-          max_tokens: 300,
-        }),
-      });
+  const { pointsOfInterestPrompt } = JSON.parse(req.body)
+  const response2 = await fetch('https://api.openai.com/v1/completions', {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      model: 'text-davinci-003',
+      prompt: pointsOfInterestPrompt,
+      temperature: 0,
+      max_tokens: 300
+    })
+  })
 
-      let pointsOfInterest = await response2.json();
+  let pointsOfInterest = await response2.json()
 
-      pointsOfInterest = pointsOfInterest.choices[0].text.split('\n');
-      pointsOfInterest = pointsOfInterest[pointsOfInterest.length - 1];
-      pointsOfInterest = pointsOfInterest.split(',');
-      const pointsOfInterestArray = pointsOfInterest.map((i) => i.trim());
+  pointsOfInterest = pointsOfInterest.choices[0].text.split('\n')
+  pointsOfInterest = pointsOfInterest[pointsOfInterest.length - 1]
+  pointsOfInterest = pointsOfInterest.split(',')
+  const pointsOfInterestArray = pointsOfInterest.map(i => i.trim())
 
-      res.status(200).json({
-        pointsOfInterest: JSON.stringify(pointsOfInterestArray),
-      });
-    } else {
-      res.status(404).json({
-        message: 'failure',
-      });
-    }
-  } catch (err) {
-    console.log(err);
-  }
+  res.status(200).json({
+    pointsOfInterest: JSON.stringify(pointsOfInterestArray)
+  })
 }
